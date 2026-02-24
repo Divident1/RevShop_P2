b71cd8126781dc473b82a5d93edee4a5af50693e
manjula <manjulakvkaki86@gmail.com>
2026-02-24 13:11:21 +0530

    added backend src folder strcture

---
+++ backend/src/main/java/com/revshop/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.revshop.controller;
+
+import com.revshop.dto.*;
+import com.revshop.service.AuthService;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return authService.login(request);
+    }
+
+    @PostMapping("/forgot-password")
+    public String forgotPassword(@RequestBody ForgotPasswordRequest request) {
+        authService.forgotPassword(request);
+        return "Reset token generated";
+    }
+
+    @PostMapping("/reset-password")
+    public String resetPassword(@RequestBody ResetPasswordRequest request) {
+        authService.resetPassword(request);
+        return "Password updated successfully";
+    }
+}
