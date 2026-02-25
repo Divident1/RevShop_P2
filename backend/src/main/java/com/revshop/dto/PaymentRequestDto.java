2de24510142439f99238a2cfc084b24a8aa040f9
PAVAN <pavankalyan.ankam@gmail.com>
2026-02-26 02:38:34 +0530

    feat: overhaul frontend styling, add cart/favorite functionality to product list, and fix backend DTO compilation errors

--- backend/src/main/java/com/revshop/dto/PaymentRequestDto.java
+++ backend/src/main/java/com/revshop/dto/PaymentRequestDto.java
@@ -2,11 +2,24 @@ package com.revshop.dto;
 
 import lombok.Data;
 
-@Data
 public class PaymentRequestDto {
 
     private String orderId;
-
     private String paymentMethod;
 
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 }
